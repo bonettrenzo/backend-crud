59651c9ce2d2fbe229de672d7934e5ec23ee068c
bonettrenzo <bonettrenzo@gmail.com>
2023-09-27 18:31:12 -0500

    token

--- src/middleware/authenticateToken.js
+++ src/middleware/authenticateToken.js
@@ -4,13 +4,12 @@ import jwt from "jsonwebtoken";
 export default function authenticateToken(req, res, next) {
 
     const bearerHeader = req.header("Authorization");
-    console.log(bearerHeader)
     if(typeof bearerHeader !== "undefined"){
         const bearerToken = bearerHeader.split(" ")[1]
         req.token = bearerToken
         next()
     }else{
-        res.sendStatus(403).json({
+        res.status(403).json({
             error: "no tienes permisos para acceder",
             success: false
         })
